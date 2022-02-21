bc2a6c795a703a99bafd591d201a749da3a519bb
Stathis96 <ioannidis.stathis@yahoo.gr>
2022-02-21 13:06:47 +0200

    Validator
    checking if email already exists beforehand

---
+++ src/modules/user/register/isEmailAlreadyExist.ts
@@ -0,0 +1,21 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface
+} from "class-validator";
+
+import { User } from "../../../entity/User";
+
+@ValidatorConstraint({ async: true })
+export class IsEmailAlreadyExistConstraint
+  implements ValidatorConstraintInterface {
+  validate(email: string) {
+    return User.findOne({ where: { email } }).then(user => {
+      if (user) return false;
+      return true;
+    });
+  }
+}
+
+}
