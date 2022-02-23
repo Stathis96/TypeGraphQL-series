add3173d780690e513a4a39db4806e819c46c082
Stathis96 <ioannidis.stathis@yahoo.gr>
2022-02-23 10:30:31 +0200

    completed

--- src/modules/utils/sendEmail.ts
+++ src/modules/utils/sendEmail.ts
@@ -13,5 +13,17 @@ export async function sendEmail(email: string, url: string) {
     }
   });
 
- 
+  const mailOptions = {
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: email, // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: `<a href="${url}">${url}</a>` // html body
+  };
+
+  const info = await transporter.sendMail(mailOptions);
+
+  console.log("Message sent: %s", info.messageId);
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
 }
